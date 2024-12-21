3786a33a75e1dbe9872c5afe544f72f336d09d3b
Евгений <info-best@bk.ru>
2024-12-21 15:03:21 +0300

    idea4

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ async def news() -> dict:
 @app.get("/user/{username}/{id}")
 
     # '''"Это прописывается когда нужно прописывать два параметра user name and id'''
-# async def news(username: str = Path(min_length=3, max_length=15, description="Enter your username"),
-#                id: int = Path(ge=0, le=100, description="Enter your id", example="74")) -> dict:
-#     return {"message": f"Hello {username} {id}"}
+async def news(username: str = Path(min_length=3, max_length=15, description="Enter your username"),
+               id: int = Path(ge=0, le=100, description="Enter your id", example="74")) -> dict:
+    return {"message": f"Hello {username} {id}"}
 
     #'''Но когда нам нужно прописать только один параметр а другой оставить без описания нужно использовать Annotated'''
-async def news_get(username: Annotated[str, Path(min_length=3, max_length=15, description="Enter your username")],
-                       id: int ):
-    return {"message": f"Hello {username} {id}"}
+# async def news_get(username: Annotated[str, Path(min_length=3, max_length=15, description="Enter your username")],
+#                        id: int ):
+#     return {"message": f"Hello {username} {id}"}
 
 @app.get("/id")
 async def news_id(username: str, age: int) -> dict:
